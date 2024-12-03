fcdf6eff58892c48b6231423a759fa78b63cf44d
manojmahi90526 <23hr1a3012@mtieat.org>
2024-12-03 10:43:26 +0530

    Add files via upload

---
+++ comparision.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class compare{
+    public static void main(String arg[]){
+    Scanner s=new Scanner(System.in);
+    int a=s.nextInt();
+    int b=s.nextInt();
+    System.out.println("a is greater than b"+(a>b));
+    System.out.println("a is lesser than b"+(a<b));
+    System.out.println("a is equals to b"+(a==b));
+    System.out.println("a is not equals to b"+(a!=b));
+    System.out.println("a is greater than equals to b"+(a>=b));
+    System.out.println("a is lesser than equals to b"+(a<=b));
+
+    }
+}
