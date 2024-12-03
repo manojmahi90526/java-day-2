8f0c9826284c9589eab6ff2dff2146934aa72c15
manojmahi90526 <23hr1a3012@mtieat.org>
2024-12-03 10:14:52 +0530

    Add files via upload

---
+++ arithmatic.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class arith{
+    public static void main(String args[]){
+        int c;
+        Scanner s=new Scanner(System.in);
+        int a=s.nextInt();
+        int b=s.nextInt();
+        c=a+b;
+        System.out.println(c);
+        c=a-b;
+        System.out.println(c);
+        c=a*b;
+        System.out.println(c);
+        c=a/b;
+        System.out.println(c);
+        c=a%b;
+        System.out.println(c);
+    }
+}
