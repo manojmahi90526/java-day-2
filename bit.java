3788dbc6ee683ab05ecf78310cabd16c1be5597e
manojmahi90526 <23hr1a3012@mtieat.org>
2024-12-03 16:08:28 +0530

    Add files via upload

---
+++ bit.java
@@ -0,0 +1,13 @@
+class bitt{
+    public static void main(String args[]){
+        int a=6,b=9;
+        if((a&b)==0)
+        {
+            System.out.println("given number is even");
+        }
+        else
+        {
+            System.out.println("given number is odd");
+        }
+        }
+    }
