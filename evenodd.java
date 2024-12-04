25a62daeb2d9e964fcc8dcdde88dbf44950a8016
manojmahi90526 <23hr1a3012@mtieat.org>
2024-12-04 10:17:13 +0530

    Add files via upload

---
+++ evenodd.java
@@ -0,0 +1,13 @@
+class bitt{
+    public static void main(String args[]){
+        int a=6;
+        if((a&1)==0)
+        {
+            System.out.println("given number is even");
+        }
+        else
+        {
+            System.out.println("given number is odd");
+        }
+        }
+    }
